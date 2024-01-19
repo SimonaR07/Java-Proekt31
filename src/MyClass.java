cd08c59c84f4e84ce8d2de6a00d39acdb6c436ea
SimonaR07 <sristomanova32@gmail.com>
2024-01-19 20:56:15 +0100

    Add new java program

---
+++ src/MyClass.java
@@ -0,0 +1,18 @@
+import Proekt1.Vehicle;
+
+public class MyClass {
+	public static void main(String[] args) {
+		Vehicle v1=new Vehicle();
+		v1.setColor("Red");
+		System.out.println(v1.getColor());
+		v1.setX(10);
+		System.out.println(v1.getX());
+		v1.setZbor("new");
+		System.out.println(v1.getZbor());
+		v1.setBroj(9);
+        System.out.println(v1.getBroj());		
+	
+
+	}
+
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,49 @@
+
+public class Vehicle {
+	private String Color;
+	private int X;
+	private String Zbor;
+	private int Broj;
+	
+	
+	public int getBroj() {
+		return Broj;
+	}
+
+	public void setBroj(int broj) {
+		Broj = broj;
+	}
+
+	public int getX() {
+		return X;
+	}
+
+	public String getZbor() {
+		return Zbor;
+	}
+
+	public void setZbor(String zbor) {
+		Zbor = zbor;
+	}
+
+	public void setX(int X) {
+		this.X = X;
+	}
+
+public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+private String color;
+
+    }
+
+
+
+
+
+
